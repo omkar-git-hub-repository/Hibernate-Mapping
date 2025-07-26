9514322fe9bebb32f9f8d4be70f114328d7ce105
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-07-26 22:49:52 +0530

    Updated

--- src/main/java/com/practise/app/App.java
+++ src/main/java/com/practise/app/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         
-        System.out.println( "Hello World!" );
+        System.out.println( "Hibernate Mapping" );
         SetDetails setDetails = new SetDetails();
         setDetails.setDetails();
     }
