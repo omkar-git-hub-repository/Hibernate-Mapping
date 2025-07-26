74c52ac756af2c4e0fb1643e04e9dd0bd305716b
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-07-26 10:58:38 +0530

    Update Hibernate configuration and add Student and StudentDoc entity classes

---
+++ src/main/java/com/practise/Entity/StudentDoc.java
@@ -0,0 +1,44 @@
+package com.practise.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class StudentDoc {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String PanNumber;
+    private String AadharNumber;
+
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getPanNumber() {
+        return PanNumber;
+    }
+    public void setPanNumber(String panNumber) {
+        PanNumber = panNumber;
+    }
+    public String getAadharNumber() {
+        return AadharNumber;
+    }
+    public void setAadharNumber(String aadharNumber) {
+        AadharNumber = aadharNumber;
+    }
+
+
+    @Override
+    public String toString() {
+        return "StudentDoc [id=" + id + ", PanNumber=" + PanNumber + ", AadharNumber=" + AadharNumber + "]";
+    }
+    
+
+}
